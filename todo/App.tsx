3453e2fe34f3ca472b444b722809ff1ba7f0a412
Jimmy Au <jjbubudi@gmail.com>
2019-01-06 13:50:42 +0800

    Add react-router-dom

--- todo/App.tsx
+++ todo/App.tsx
@@ -1,8 +1,36 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { Button } from "@material-ui/core";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+const Index = () => <h2>Home</h2>;
+const About = () => <h2>About</h2>;
+const Users = () => <h2><Button variant="contained" color="primary">Hello World</Button></h2>;
+
+const AppRouter = () => (
+  <Router>
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about/">About</Link>
+          </li>
+          <li>
+            <Link to="/users/">Users</Link>
+          </li>
+        </ul>
+      </nav>
+      <Route path="/" exact component={Index} />
+      <Route path="/about/" component={About} />
+      <Route path="/users/" component={Users} />
+    </div>
+  </Router>
+);
 
 ReactDOM.render(
-  <Button variant="contained" color="primary">Hello World</Button>,
+  <AppRouter />,
   document.getElementById("root")
 );
