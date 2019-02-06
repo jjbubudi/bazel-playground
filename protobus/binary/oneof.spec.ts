b2679f5b3f68949c2a2c10fab70c2d60f98d1534
Jimmy Au <jjbubudi@gmail.com>
2019-02-06 12:16:14 +0800

    Unit tests for oneof

---
+++ protobus/binary/oneof.spec.ts
@@ -0,0 +1,18 @@
+import { oneof } from './oneof';
+import { int32Field } from './integral';
+import { FieldType } from '../language/schema';
+
+describe('Oneof', () => {
+  it('should decode oneof message', () => {
+    const field = oneof({
+      foo: int32Field(1),
+      bar: int32Field(2)
+    });
+    [
+      { tag: 1, bytes: [0x05], value: <FieldType<typeof field>>{ type: 'foo', value: 5 } },
+      { tag: 2, bytes: [0x96, 0x01], value: <FieldType<typeof field>>{ type: 'bar', value: 150 } }
+    ].forEach(({ tag, bytes, value }) => {
+      expect(field.decode(tag, 0, new Uint8Array(bytes))).toEqual([value, bytes.length]);
+    });
+  });
+});
